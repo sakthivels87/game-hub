47ae357e0718fe09ac20574dfbd9cad53debeb82
Sakthivel <sakthi.samuthiram@gmail.com>
2023-05-22 19:00:27 +0530

    use static data to implement platforms and genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,12 +1,13 @@
-import useData from "./useData";
+import Genres from "../data/genre";
 
 export interface Genre {
-    id:number;
-    name:string;
-    slug:string;
-    image_background:string;
+  id: number;
+  name: string;
+  slug: string;
+  image_background: string;
 }
 
-const useGenres = () => useData<Genre>("/genres")
+//const useGenres = () => useData<Genre>("/genres")
+const useGenres = () => ({ data: Genres, isLoading: false, error: null });
 
 export default useGenres;
